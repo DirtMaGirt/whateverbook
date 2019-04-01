6ce57981d0a7e3fca47bb345e0a014cad41bcd98
Jamal Ames <Jamal.ames@gmail.com>
2019-04-01 01:48:41 -0400

    Completed the console log and display all functions; moving onto the search function.

--- data.js
+++ data.js
@@ -1,3 +1,16 @@
-const data = [
-    
+const data = [{
+        Name: 'Tim',
+        Age: 47,
+        Fun: true,
+    },
+    {
+        Name: 'Davy',
+        Age: 34,
+        Fun: false,
+    },
+    {
+        Name: 'Davy',
+        Age: 34,
+        Fun: false,
+    }
 ]
--- main.js
+++ main.js
@@ -1,1 +1,52 @@
 window.onload = init;
+
+function init() {
+    // Button 1: the Search button. This button will search the array for the object and return. 
+    document.querySelector('#search')
+        .addEventListener('click', searchList);
+    // Button 2: T
+    document.querySelector('#display-all')
+        .addEventListener('click', displayList);
+
+
+}
+
+function displayList(event) {
+    event.preventDefault()
+    let i;
+    for (let i in data) {
+        console.log("user");
+        for (let j in data[i]) {
+
+
+            function addElement() {
+                let divNew = document.createElement("div");
+                let divContent = document.createTextNode(data[i][j]);
+                divNew.appendChild(divContent);
+                let divBase = document.getElementById("wrapper");
+                document.body.insertBefore(divNew, divBase);
+
+
+            }
+            addElement()
+        }
+
+
+
+    }
+
+
+}
+
+function searchList(event) {
+    event.preventDefault()
+    let i;
+    for (let i in data) {
+        console.log("user");
+        for (let j in data[i]) {
+            console.log(" " + data[i][j]);
+        }
+
+    }
+
+}
